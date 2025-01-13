8420b88cf25aec63c30e0722bad64fbaa47f588f
Scott McCoy <scotthmccoy@gmail.com>
2025-01-13 14:39:13 -0800

    feat: Added a loading placeholder image, switched to scaleToFit

--- ANF Code Test/View/ExploreCardCell.swift
+++ ANF Code Test/View/ExploreCardCell.swift
@@ -14,7 +14,16 @@ struct ExploreCardCell: View {
     var body: some View {
         VStack {
             Text(product.title)
-            CachedAsyncImage(url: product.backgroundImageUrl)
+            CachedAsyncImage(url: product.backgroundImageUrl) { phase in
+                switch phase {
+                    case .success(let image):
+                        image.resizable()
+                    default:
+                        Image("Loading")
+                            .resizable()
+                }
+            }
+            .scaledToFit()
         }
     }
 }
@@ -22,7 +31,7 @@ struct ExploreCardCell: View {
 #Preview {
     ExploreCardCell(product: Product(
             title: "Title",
-            backgroundImageUrl: URL(string:  "https://img.abercrombie.com/is/image/anf/KIC_155-4713-00030-278_model6.jpg?policy=product-medium")!,
+            backgroundImageUrl: URL(string:  "https://img.abercrombie.com/is/image/anf/KIC_155-4713-00030-278_model6.jpg2?policy=product-medium")!,
             content: [],
             promoMessage: "Promo Message",
             topDescription: "Top Desc",
