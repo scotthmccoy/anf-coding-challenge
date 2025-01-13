75fddbee7f57b597c4d294fadef0943a32cf5d86
Scott McCoy <scotthmccoy@gmail.com>
2025-01-13 10:18:06 -0800

    feat: Switch to SwiftUI
    Removed AppDelegate, added App

---
+++ ANF Code Test/App.swift
@@ -0,0 +1,59 @@
+//
+//  App.swift
+//  ANF Code Test
+//
+//  Created by Scott McCoy on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ANFCodeTestApp: App {
+    
+    var isUnitTest = false
+    
+    init() {
+        AppLog("Launching...")
+        #if DEBUG
+        
+        let args = extractArgs()
+        
+        AppLog("Launch Args: \(args)")
+        
+        // Determine if Unit Tests are running. The UI will not display.
+        isUnitTest = args.keys.contains("UNIT_TEST")
+        
+        // Override the API URL (used in UI Testing to set up different testing scenarios)
+        if let overrideApiUrl = args["OVERRIDE_API_URL"] {
+            
+        }
+
+        #endif
+    }
+    
+    func extractArgs() -> [String: String] {
+        var ret = [String: String]()
+        for arg in ProcessInfo.processInfo.arguments {
+            let tokens = arg.split(separator: "=")
+            if let key = tokens.first, let value = tokens.last {
+                ret[String(key)] = String(value)
+            } else {
+                ret[arg] = ""
+            }
+        }
+        
+        return ret
+    }
+    
+
+    var body: some Scene {
+        WindowGroup {
+            if (!isUnitTest) {
+                NavigationStack {
+                    ExploreCardView()
+                        .preferredColorScheme(.dark)
+                }
+            }
+        }
+    }
+}
---
+++ ANF Code Test/ExploreCardView.swift
@@ -0,0 +1,14 @@
+//
+//  ExploreCardView.swift
+//  ANF Code Test
+//
+//  Created by Scott McCoy on 1/13/25.
+//
+
+import SwiftUI
+
+struct ExploreCardView: View {
+    var body : some View {
+        Text("Test")
+    }
+}
