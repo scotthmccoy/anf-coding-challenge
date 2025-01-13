044b80e2ca18490b912a7f6e7df900258f86af3a
Scott McCoy <scotthmccoy@gmail.com>
2025-01-13 14:11:24 -0800

    feat: Basic UI in SwiftUI displaying nicely. chore: Renamed images in xcassets to include file extension; Image wasn't able to find them without it.
    Also, removed UIKit classes, added basic viewModel

---
+++ ANF Code Test/View/ExploreCardView.swift
@@ -0,0 +1,25 @@
+//
+//  ExploreCardView.swift
+//  ANF Code Test
+//
+//  Created by Scott McCoy on 1/13/25.
+//
+
+import SwiftUI
+
+struct ExploreCardView: View {
+    
+    @StateObject var exploreCardViewModel = ExploreCardViewModel()
+    
+    var body : some View {
+        ScrollView {
+            ForEach(exploreCardViewModel.products, id: \.self) { product in
+                ExploreCardCell(product: product)
+            }
+        }
+    }
+}
+
+#Preview {
+    ExploreCardView()
+}
