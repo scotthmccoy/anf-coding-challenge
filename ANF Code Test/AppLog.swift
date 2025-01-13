7398b2247c8974b429a7e3afff54dfeec8a5649b
Scott McCoy <scotthmccoy@gmail.com>
2025-01-13 10:12:49 -0800

    feat: Added AppLog

---
+++ ANF Code Test/AppLog.swift
@@ -0,0 +1,18 @@
+//
+//  AppLog.swift
+//  ANF Code Test
+//
+//  Created by Scott McCoy on 1/13/25.
+//
+
+func AppLog(
+    _ message: String = "",
+    file: StaticString = #file,
+    line: UInt = #line,
+    column: UInt = #column,
+    function: StaticString = #function
+) {
+    //Tokenize file by "/", get the last element
+    let shortFile = file.description.components(separatedBy: "/").last ?? "NO FILE"
+    print("📱\(Date()) \(shortFile) \(function) [Line \(line)] \(message)")
+}
