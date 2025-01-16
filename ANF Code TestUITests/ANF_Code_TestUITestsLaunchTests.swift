71e083ebae2ab8c1bbdeb54aeae3dc073a3eaa5a
Scott McCoy <scotthmccoy@gmail.com>
2025-01-16 11:19:43 -0800

    test: Stubbed UI Tests

---
+++ ANF Code TestUITests/ANF_Code_TestUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ANF_Code_TestUITestsLaunchTests.swift
+//  ANF Code TestUITests
+//
+//  Created by Scott McCoy on 1/16/25.
+//
+
+import XCTest
+
+final class ANF_Code_TestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
